7856345071f300c3f95510c28e9759ba30116699
Fletcher Woodruff <fwood@amazon.com>
2025-08-13 10:41:37 -0700

    Run shellcheck on PRs
    Limit to error severity since we have a lot of existing warnings.
    Fix all existing errors.

--- templates/al2023/provisioners/install-worker.sh
+++ templates/al2023/provisioners/install-worker.sh
@@ -136,7 +136,7 @@ if ! [[ ${ISOLATED_REGIONS} =~ $BINARY_BUCKET_REGION ]]; then
     --retry 10 \
     --retry-delay 1 \
     -L "https://awscli.amazonaws.com/awscli-exe-linux-${MACHINE}.zip" -o "${AWSCLI_DIR}/awscliv2.zip"
-  unzip -q "${AWSCLI_DIR}/awscliv2.zip" -d ${AWSCLI_DIR}
+  unzip -q "${AWSCLI_DIR}/awscliv2.zip" -d "${AWSCLI_DIR}"
   sudo "${AWSCLI_DIR}/aws/install" --bin-dir /bin/ --update
 else
   echo "Installing awscli package"
